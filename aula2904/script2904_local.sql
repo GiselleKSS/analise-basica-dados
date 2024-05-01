494f930def1143c7aa20c89a10550c4d695bdf39
Perkles <gisellekarinna@outlook.com>
2024-04-30 23:35:43 -0300

    Aula3004 Procedures, Temporary Tables e Case

---
+++ aula2904/script2904_local.sql
@@ -0,0 +1,26 @@
+start transaction;
+insert into empregado(cod_depto, nome, dt_nascimento, sexo, dt_admissao, civil, salario) values(1,'VICTOR SENA ICOMA','1981-10-31','M','2021-02-02','C',2800);
+insert into dependente(cod_emp, nome, dt_nascimento, sexo) values(17,'LETÍCIA ICOMA','2003-12-08','F');
+
+rollback;
+commit;
+
+select * from empregado;
+select * from dependente;
+
+drop trigger gatilho_exclusao_emp;
+
+delimiter &&
+create trigger gatilho_exclusao_emp before delete
+on empregado
+for each row 
+	begin
+    delete from dependente where dep.cod_emp = old.cod_emp;
+	end
+&&
+
+start transaction;
+delete from empregado where cod_emp=17;
+
+start transaction;
+delete from dependente where cod_emp=17;
